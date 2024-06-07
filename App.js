1df623e0af318dcbb3280aff922ff6855d8372e7
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2024-06-07 22:20:44 +0530

    Igniting our app

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
-const parent = React.createElement("div",{id:"parent"},
-[
-React.createElement("h1",{id:"child"},"I am child1"),
-React.createElement("h2",{id:"child"},"I am child2")
-])
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const parent = React.createElement("h1",{id:"parent"},"Hello World from React!")
 
 //JSX
-
 const heading = React.createElement("h1",{ id:"heading"}, "Hello world from React!");
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); //object
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 #Namaste React 🚀
+
+# Parcel
+- Dev Build
+- Local server
+- HMR = Hot Module Replacement
+- File Watching Alogorithm - written in C++
+- Image optimization
+- Minification
+- Bundling
+- Compressing
+- Consistent Hashing
+- Code Splitting
+- Differential Bundling - support older browser
+- Dignostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused code
+- Different dev and prod bundles
