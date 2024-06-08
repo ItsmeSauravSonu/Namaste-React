8a52150e7506399e7c0f36b9caacf969f1de506b
SAURAV KUMAR SONU <57924568+ItsmeSauravSonu@users.noreply.github.com>
2024-06-08 13:58:14 +0530

    components

--- App.js
+++ App.js
@@ -1,9 +1,30 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom/client"; 
 
-const parent = React.createElement("h1",{id:"parent"},"Hello World from React!")
+//React.createElement =>React Element (Object) =>HTML element (render)
 
-//JSX
-const heading = React.createElement("h1",{ id:"heading"}, "Hello world from React!");
+//JSX is not HTML in JS, it's like html syntax, xml like syntax
+// JSX => Babel transpiles it into React.createElement => React.createElement =>React Element (Object) =>HTML element (render)
+const Title = () => (
+    <h1  className="heading">
+        Namaste React using JSX🚀
+    </h1>
+);
+
+
+//React components
+//class based components --> old way
+//Functional components  --> New
+
+// React functional componenet: A function that returns piece of JSX code i.e A js function that returns react Element
+
+//component composition
+const HeadingComponent = () => (
+    <div id="container">
+        <Title/>
+        <h2 className="heading">Namaste React Functional Component 🚀</h2>;
+    </div>
+)
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent); //object
+root.render(<HeadingComponent/>);
